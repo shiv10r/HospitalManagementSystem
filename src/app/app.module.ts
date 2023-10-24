40c0aa9e684743370fe833ea71ebbcd0fe89b69a
Shivanshu Rai <shr1030cd@gmail.com>
2023-10-24 23:39:14 +0530

    Added the HMSUI

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
+import { MaterialModule } from './material/material/material.module';
+import { DoctorComponent } from './component/dashboard/doctor/doctor.component';
+import { PatientComponent } from './component/dashboard/patient/patient.component';
+import { SidebarComponent } from './component/dashboard/sidebar/sidebar.component';
+import { AddDoctorComponent } from './component/dashboard/doctor/add-doctor/add-doctor.component';
+import { DeleteDoctorComponent } from './component/dashboard/doctor/delete-doctor/delete-doctor.component';
+import { ViewDoctorComponent } from './component/dashboard/doctor/view-doctor/view-doctor.component';
+import { AddPatientComponent } from './component/dashboard/patient/add-patient/add-patient.component';
+import { DeletePatientComponent } from './component/dashboard/patient/delete-patient/delete-patient.component';
+import { ViewPatientComponent } from './component/dashboard/patient/view-patient/view-patient.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; // for reactive forms
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DoctorComponent,
+    PatientComponent,
+    SidebarComponent,
+    AddDoctorComponent,
+    DeleteDoctorComponent,
+    ViewDoctorComponent,
+    AddPatientComponent,
+    DeletePatientComponent,
+    ViewPatientComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: [AddDoctorComponent]
+})
+export class AppModule { }
